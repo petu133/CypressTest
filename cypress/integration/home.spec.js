f6adfab4df9c1057c06941a91ead2bb7b8d45b93
petu133 <dario.snd@gmail.com>
2022-12-18 17:19:51 -0300

    refactor including should command + fix amiss node modules push

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -3,6 +3,9 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit("http://localhost:3000")
-    cy.get("h1").contains("Cypress")
+    cy.get("h1")
+    .should("exist")
+    .contains("Cypress")
   })
 })
+
