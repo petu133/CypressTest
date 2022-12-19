670c92dd202db6b11e8a07a8932c1f11a2b76e09
petu133 <dario.snd@gmail.com>
2022-12-19 10:40:13 -0300

    implement before each hook to reach the page url

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,15 +1,17 @@
 /// <reference types="Cypress" />
 
 describe('empty spec', () => {
-  it('h1 contains the text required', () => {
+  beforeEach(() =>{
     cy.visit("http://localhost:3000")
+  })
+
+  it('h1 contains the text required', () => {
     cy.get('[data-test="hero-heading"]')
     .should("exist")
     .contains("Cypress")
   })
 
-  it.only('check that Courses are correctly displayed ', () => {
-    cy.visit("http://localhost:3000")
+  it('check that Courses are correctly displayed ', () => {
     cy.get("dt").eq(0).contains("4 Courses")
   })
 
