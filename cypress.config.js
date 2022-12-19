82809e728de2efaea06ab30fb8c0322e10624562
petu133 <dario.snd@gmail.com>
2022-12-19 10:27:45 -0300

    resize default viewport display - Check courses are properly displayed

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   component: {
-    viewportHeight: 1000,
-    viewportWidth: 1280,
+    viewportHeight: 600,
+    viewportWidth: 1000,
     devServer: {
       framework: "next",
       bundler: "webpack",
--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,11 +1,18 @@
 /// <reference types="Cypress" />
 
 describe('empty spec', () => {
-  it('passes', () => {
+  it('h1 contains the text required', () => {
     cy.visit("http://localhost:3000")
     cy.get('[data-test="hero-heading"]')
     .should("exist")
     .contains("Cypress")
   })
-})
+
+  it('check that Courses are correctly displayed ', () => {
+    cy.visit("http://localhost:3000")
+    cy.get("dt").eq(0).contains("4 Courses")
+  })
+
+})  
+
 
