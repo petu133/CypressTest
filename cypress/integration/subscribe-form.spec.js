c948a8f99dab0be98868681d16929a6346a80f0c
petu133 <dario.snd@gmail.com>
2022-12-21 13:37:48 -0300

    testing form with invalid email address

--- cypress/integration/subscribe-form.spec.js
+++ cypress/integration/subscribe-form.spec.js
@@ -11,4 +11,11 @@ describe('empty spec', () => {
       cy.getByData("success-message")
       .should("exist").contains("tom@aol.com")
     })
+
+    it('impede registration wwith an invalid mail ', () => {
+      cy.getByData("email-input").type("invalidmail@abc.com")
+      cy.getByData("submit-button").click()
+      cy.getByData("success-message")
+      .should("not.exist")
+    })
 })  
